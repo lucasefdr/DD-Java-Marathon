47366e564dbabdd80d85144405bd7b4ca7fca16a
Lucas <lcseferreira@hotmail.com>
2022-10-27 11:36:41 -0300

    Comments in Java

---
+++ src/edu/lucas/marathon/introduction/A01Comments.java
@@ -0,0 +1,19 @@
+package edu.lucas.marathon.introduction;
+
+public class A01Comments {
+    
+    //  comentário em linha
+
+    /*
+        comentário de múltiplas linhas
+    */
+
+    /**
+     *  comentário <b>javadoc</b> <br>
+     *  métodos públicos devem haver comentários <b>javadoc</b>
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
