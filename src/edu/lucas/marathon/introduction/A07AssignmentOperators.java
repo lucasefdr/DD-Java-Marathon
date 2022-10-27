8577cb568a30897d1974d81635d9e35d937540cb
Lucas <lcseferreira@hotmail.com>
2022-10-27 15:22:18 -0300

    Assignment operators

---
+++ src/edu/lucas/marathon/introduction/A07AssignmentOperators.java
@@ -0,0 +1,17 @@
+package edu.lucas.marathon.introduction;
+
+public class A07AssignmentOperators {
+    public static void main(String[] args) {
+        System.out.println("Assignment Operators");
+        // Operadores de Atribuição: +=, -=, *=, /=
+        var bonus = 1800F;
+        bonus *= 1.2;
+
+        System.out.println(bonus);
+
+        var contador = 1;
+        contador++;
+
+        System.out.println(contador);
+    }
+}
