78ba391eb6b370c509bec73388a970ef05b8068c
Lucas <lcseferreira@hotmail.com>
2022-10-31 14:54:14 -0300

    Conditional structures - Ternary operator

---
+++ src/edu/lucas/marathon/introduction/A10ConditionalStructuresTernary.java
@@ -0,0 +1,13 @@
+package edu.lucas.marathon.introduction;
+
+public class A10ConditionalStructuresTernary {
+    public static void main(String[] args) {
+        // Operador ternário -> (condicao) ? true : false;
+        var salary = 6000.0D;
+        var messageOk = "Irei fazer uma doação de R$ 500,00";
+        var messageNotOk = "Não irei fazer uma doação de R$ 500,00";
+
+        var resultMessage = (salary > 5000) ? messageOk : messageNotOk;
+        System.out.println(resultMessage);
+    }
+}
