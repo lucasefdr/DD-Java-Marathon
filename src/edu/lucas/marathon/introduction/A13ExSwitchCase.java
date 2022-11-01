8eb34287c6b93e013ee263c014bf16d882643b15
Lucas <lcseferreira@hotmail.com>
2022-11-01 07:50:49 -0300

    Ex. for

--- src/edu/lucas/marathon/introduction/A13ExSwitchCase.java
+++ src/edu/lucas/marathon/introduction/A13ExSwitchCase.java
@@ -2,7 +2,7 @@ package edu.lucas.marathon.introduction;
 
 import java.util.Scanner;
 
-public class A13Ex03SwitchCase {
+public class A13ExSwitchCase {
     public static void main(String[] args) {
         // Dados os valores de 1 a 7, imprima se é dia útil ou final de semana
         // Considerando 1 como domingo
---
+++ src/edu/lucas/marathon/introduction/A15ExFor.java
@@ -0,0 +1,10 @@
+package edu.lucas.marathon.introduction;
+
+public class A15ExFor {
+    public static void main(String[] args) {
+        // Exiba todos números pares até 1000
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) System.out.println(i);
+        }
+    }
+}
