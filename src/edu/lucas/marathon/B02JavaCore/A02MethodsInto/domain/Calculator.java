d300cc50064fe95c7cbae75d9b0fc02e7e2670ad
Lucas <lucas.efdrosa@gmail.com>
2022-11-08 19:49:08 -0300

    Methods pt. 3 - return

--- src/edu/lucas/marathon/B02JavaCore/A02MethodsInto/domain/Calculator.java
+++ src/edu/lucas/marathon/B02JavaCore/A02MethodsInto/domain/Calculator.java
@@ -4,6 +4,8 @@ package edu.lucas.marathon.B02JavaCore.A02MethodsInto.domain;
  * Classe criada para treinamento de <b>métodos</b> (ações)
  */
 public class Calculator {
+
+    // Nosso métodos recebem parâmetros
     public double sum(double numberOne, double numberTwo) {
         return numberOne + numberTwo;
     }
@@ -16,4 +18,8 @@ public class Calculator {
         return numberOne * numberTwo;
     }
 
+    public double division(double numberOne, double numberTwo) throws Exception {
+        if (numberTwo == 0) throw new Exception("Can't divide by 0");
+        return numberOne * numberTwo;
+    }
 }
--- src/edu/lucas/marathon/B02JavaCore/A02MethodsInto/test/CalculatorTest.java
+++ src/edu/lucas/marathon/B02JavaCore/A02MethodsInto/test/CalculatorTest.java
@@ -3,14 +3,17 @@ package edu.lucas.marathon.B02JavaCore.A02MethodsInto.test;
 import edu.lucas.marathon.B02JavaCore.A02MethodsInto.domain.Calculator;
 
 public class CalculatorTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Calculator calculator = new Calculator();
 
         double numberOne = 10;
         double numberTwo = 60;
 
-        // passando parâmetros para o nosso método
+        // Passando argumentos para o nosso método
         double sum = calculator.sum(numberOne, numberTwo);
         System.out.println(sum);
+
+        double division = calculator.division(10, 2);
+        System.out.println(division);
     }
 }
