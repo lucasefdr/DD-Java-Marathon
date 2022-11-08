e5b417ec261bc45761e5a6e60fd483ffd398dbaa
Lucas <lcseferreira@hotmail.com>
2022-11-08 15:58:48 -0300

    Arrays - introduction

--- src/edu/lucas/marathon/introduction/A16ExBreak.java
+++ src/edu/lucas/marathon/introduction/A16ExBreak.java
@@ -4,12 +4,12 @@ public class A16ExBreak {
     // Dado o valor de um carro, descubra em quantas vezes ele pode ser parcelado
     // Condição: valorParcela >= 1000
     public static void main(String[] args) {
-        var carValue = 50000;
+        var carValue = 50000D;
 
         for (var installments = 1; installments <= carValue; installments++) {
             var installmentValue = carValue / installments;
 
-            if (installmentValue <= 1000) break;
+            if (installmentValue < 1000D) break;
 
             System.out.println("Installment: " + installments + " - R$ " + installmentValue);
         }
---
+++ src/edu/lucas/marathon/introduction/A17Arrays01.java
@@ -0,0 +1,33 @@
+package edu.lucas.marathon.introduction;
+
+import java.util.Arrays;
+
+public class A17Arrays01 {
+    /**
+     *  <b>Arrays unidimensiosais</b>: arrays são referências para espaços em memória <br>
+     *  Variáveis do tipo <b>ref</b> (arrays são considerados objetos na memória)
+     */
+    public static void main(String[] args) {
+        // criando um array com 3 espaços em memória
+        int[] ages = new int[3];
+
+        // referência do objeto em memória
+        System.out.println(ages);
+
+        // array vazio com o toString
+        System.out.println(Arrays.toString(ages));
+
+        int age1 = 14;
+        int age2 = 53;
+        int age3 = 26;
+
+        // populando o array - arrays são indexados
+        ages[0] = age1;
+        ages[1] = age2;
+        ages[2] = age3;
+
+        for (int age : ages) {
+            System.out.println("Age: " + age);
+        }
+    }
+}
